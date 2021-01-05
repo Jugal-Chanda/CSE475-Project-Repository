0b72d334221133fb50b64d55b18e287224d602a1
jugal-chanda <jugalchanda7@gmail.com>
2021-01-05 14:55:23 +0600

    Update code

--- Source Code and DataSet/sourceCode/main.py
+++ Source Code and DataSet/sourceCode/main.py
@@ -30,12 +30,12 @@ while True:
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     num_faces = boundingBox.detectMultiScale(grayImg, scaleFactor=1.3, minNeighbors=5)
     for (x, y, w, h) in num_faces:
-        cv2.rectangle(img, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 1)
         grayImg = grayImg[y:y + h, x:x + w]
         croppedImg = np.expand_dims(np.expand_dims(cv2.resize(grayImg, (48, 48)), -1), 0)
         emotion_prediction = model.predict(croppedImg)
         maxIndex = int(np.argmax(emotion_prediction))
-        cv2.putText(img, emotions[maxIndex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(img, emotions[maxIndex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     cv2.imshow('Vedio', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
